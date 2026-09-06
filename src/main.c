ea4a58cc93a1200525d601d856413354dacb2f3c
L1ON <lionnapp1@gmail.com>
2026-09-06 04:07:52 +0300

    Fix browser content area sizing and make navigation toolbar visible

--- src/main.c
+++ src/main.c
@@ -58,12 +58,8 @@ static gchar *make_url(const gchar *s) {
         g_free(t);
         return g_strdup(HOME_URL);
     }
-    if (g_str_has_prefix(t, "http://") ||
-        g_str_has_prefix(t, "https://") ||
-        g_str_has_prefix(t, "file://") ||
-        g_str_has_prefix(t, "about:")) {
-        return t;
-    }
+    if (g_str_has_prefix(t, "http://") || g_str_has_prefix(t, "https://") ||
+        g_str_has_prefix(t, "file://") || g_str_has_prefix(t, "about:")) return t;
     if (!strchr(t, ' ') && strchr(t, '.')) {
         gchar *r = g_strconcat("https://", t, NULL);
         g_free(t);
@@ -84,40 +80,12 @@ static void go_address(NexoApp *a) {
     g_free(u);
 }
 
-static void address_activate(GtkEntry *e, gpointer d) {
-    (void)e;
-    go_address(d);
-}
-
-static void back_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    NexoTab *t = current(d);
-    if (t) webkit_web_view_go_back(t->view);
-}
-
-static void forward_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    NexoTab *t = current(d);
-    if (t) webkit_web_view_go_forward(t->view);
-}
-
-static void reload_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    NexoTab *t = current(d);
-    if (t) webkit_web_view_reload(t->view);
-}
-
-static void stop_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    NexoTab *t = current(d);
-    if (t) webkit_web_view_stop_loading(t->view);
-}
-
-static void home_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    NexoTab *t = current(d);
-    if (t) webkit_web_view_load_uri(t->view, HOME_URL);
-}
+static void address_activate(GtkEntry *e, gpointer d) { (void)e; go_address(d); }
+static void back_cb(GtkButton *b, gpointer d) { (void)b; NexoTab *t = current(d); if (t) webkit_web_view_go_back(t->view); }
+static void forward_cb(GtkButton *b, gpointer d) { (void)b; NexoTab *t = current(d); if (t) webkit_web_view_go_forward(t->view); }
+static void reload_cb(GtkButton *b, gpointer d) { (void)b; NexoTab *t = current(d); if (t) webkit_web_view_reload(t->view); }
+static void stop_cb(GtkButton *b, gpointer d) { (void)b; NexoTab *t = current(d); if (t) webkit_web_view_stop_loading(t->view); }
+static void home_cb(GtkButton *b, gpointer d) { (void)b; NexoTab *t = current(d); if (t) webkit_web_view_load_uri(t->view, HOME_URL); }
 
 static void select_tab(NexoApp *a, guint i) {
     if (i >= a->pages->len) return;
@@ -132,20 +100,18 @@ static void select_tab(NexoApp *a, guint i) {
 
 static void tab_click(GtkButton *b, gpointer d) {
     NexoTab *t = g_object_get_data(G_OBJECT(b), "nexo-tab");
-    select_tab(d, index_of(d, t));
+    if (t) select_tab(d, index_of(d, t));
 }
 
 static void title_changed(GObject *o, GParamSpec *p, gpointer d) {
-    (void)o;
-    (void)p;
+    (void)o; (void)p;
     NexoTab *t = d;
     const gchar *s = webkit_web_view_get_title(t->view);
     gtk_label_set_text(GTK_LABEL(t->label), (s && *s) ? s : "Yeni Sekme");
 }
 
 static void uri_changed(GObject *o, GParamSpec *p, gpointer d) {
-    (void)o;
-    (void)p;
+    (void)o; (void)p;
     NexoTab *t = d;
     if (current(t->app) == t) {
         gchar *u = shown_uri(webkit_web_view_get_uri(t->view));
@@ -170,25 +136,19 @@ static gboolean policy(WebKitWebView *v, WebKitPolicyDecision *d,
     (void)v;
     NexoApp *a = data;
     if (type != WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION &&
-        type != WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION) {
-        return FALSE;
-    }
-
+        type != WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION) return FALSE;
     WebKitNavigationPolicyDecision *n = WEBKIT_NAVIGATION_POLICY_DECISION(d);
     WebKitNavigationAction *x = webkit_navigation_policy_decision_get_navigation_action(n);
     WebKitURIRequest *r = webkit_navigation_action_get_request(x);
     const gchar *uri = webkit_uri_request_get_uri(r);
-
     if (type == WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION) {
         new_tab(a, uri);
         webkit_policy_decision_ignore(d);
         return TRUE;
     }
-
     guint modifiers = webkit_navigation_action_get_modifiers(x);
     WebKitNavigationType navigation_type = webkit_navigation_action_get_navigation_type(x);
-    if ((modifiers & GDK_CONTROL_MASK) &&
-        navigation_type == WEBKIT_NAVIGATION_TYPE_LINK_CLICKED) {
+    if ((modifiers & GDK_CONTROL_MASK) && navigation_type == WEBKIT_NAVIGATION_TYPE_LINK_CLICKED) {
         new_tab(a, uri);
         webkit_policy_decision_ignore(d);
         return TRUE;
@@ -199,15 +159,13 @@ static gboolean policy(WebKitWebView *v, WebKitPolicyDecision *d,
 static void css(void) {
     GtkCssProvider *p = gtk_css_provider_new();
     gtk_css_provider_load_from_string(p,
-        "headerbar { padding: 6px 10px; min-height: 52px; }"
+        "#toolbar { padding: 7px 10px; }"
         ".nav { min-width: 38px; min-height: 38px; padding: 0; }"
         ".address { min-height: 40px; border-radius: 20px; padding: 0 16px; }"
         ".tabs { padding: 4px 8px 0; }"
         ".tab-button { min-height: 36px; padding: 3px 7px; }"
         ".tab-button label { max-width: 190px; }");
-    gtk_style_context_add_provider_for_display(
-        gdk_display_get_default(), GTK_STYLE_PROVIDER(p),
-        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    gtk_style_context_add_provider_for_display(gdk_display_get_default(), GTK_STYLE_PROVIDER(p), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
     g_object_unref(p);
 }
 
@@ -216,6 +174,8 @@ static void new_tab(NexoApp *a, const gchar *u) {
     t->app = a;
     t->id = a->next_tab_id++;
     t->view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    gtk_widget_set_hexpand(GTK_WIDGET(t->view), TRUE);
+    gtk_widget_set_vexpand(GTK_WIDGET(t->view), TRUE);
 
     WebKitSettings *s = webkit_web_view_get_settings(t->view);
     webkit_settings_set_enable_javascript(s, TRUE);
@@ -228,6 +188,9 @@ static void new_tab(NexoApp *a, const gchar *u) {
     webkit_settings_set_enable_media_stream(s, TRUE);
 
     t->page = gtk_scrolled_window_new();
+    gtk_widget_set_hexpand(t->page, TRUE);
+    gtk_widget_set_vexpand(t->page, TRUE);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(t->page), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(t->page), GTK_WIDGET(t->view));
     t->stack_name = g_strdup_printf("tab-%u", t->id);
     gtk_stack_add_named(GTK_STACK(a->stack), t->page, t->stack_name);
@@ -240,6 +203,7 @@ static void new_tab(NexoApp *a, const gchar *u) {
     gtk_widget_set_size_request(t->label, 120, -1);
     t->close = gtk_button_new_from_icon_name("window-close-symbolic");
     gtk_widget_add_css_class(t->close, "flat");
+    gtk_widget_set_tooltip_text(t->close, "Sekmeyi kapat");
     gtk_box_append(GTK_BOX(box), t->label);
     gtk_box_append(GTK_BOX(box), t->close);
     gtk_button_set_child(GTK_BUTTON(t->button), box);
@@ -259,27 +223,22 @@ static void new_tab(NexoApp *a, const gchar *u) {
     select_tab(a, a->pages->len - 1);
 }
 
-static void new_tab_cb(GtkButton *b, gpointer d) {
-    (void)b;
-    new_tab(d, HOME_URL);
-}
+static void new_tab_cb(GtkButton *b, gpointer d) { (void)b; new_tab(d, HOME_URL); }
 
 static void close_tab(GtkButton *b, gpointer d) {
     NexoApp *a = d;
     NexoTab *t = g_object_get_data(G_OBJECT(b), "nexo-tab");
+    if (!t) return;
     guint i = index_of(a, t);
-
     if (a->pages->len == 1) {
         webkit_web_view_load_uri(t->view, HOME_URL);
         return;
     }
-
     gtk_stack_remove(GTK_STACK(a->stack), t->page);
     gtk_box_remove(GTK_BOX(a->tabs), t->button);
     g_ptr_array_remove_index(a->pages, i);
     g_free(t->stack_name);
     g_free(t);
-
     select_tab(a, i < a->pages->len ? i : a->pages->len - 1);
 }
 
@@ -292,25 +251,24 @@ static void activate(GtkApplication *g, gpointer u) {
     a->window = gtk_application_window_new(g);
     gtk_window_set_title(GTK_WINDOW(a->window), "Nexo Browser");
     gtk_window_set_default_size(GTK_WINDOW(a->window), 1280, 800);
+    gtk_window_set_resizable(GTK_WINDOW(a->window), TRUE);
     css();
 
-    GtkWidget *hb = gtk_header_bar_new();
-    gtk_window_set_titlebar(GTK_WINDOW(a->window), hb);
+    GtkWidget *root = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget *toolbar = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4);
+    gtk_widget_set_name(toolbar, "toolbar");
 
-    GtkWidget *nav = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 3);
     a->back = gtk_button_new_from_icon_name("go-previous-symbolic");
     a->forward = gtk_button_new_from_icon_name("go-next-symbolic");
     a->reload = gtk_button_new_from_icon_name("view-refresh-symbolic");
     a->stop = gtk_button_new_from_icon_name("process-stop-symbolic");
     GtkWidget *home = gtk_button_new_from_icon_name("go-home-symbolic");
     GtkWidget *plus = gtk_button_new_from_icon_name("tab-new-symbolic");
-    GtkWidget *buttons[] = { a->back, a->forward, a->reload, a->stop, home, plus };
-
+    GtkWidget *buttons[] = {a->back, a->forward, a->reload, a->stop, home, plus};
     for (guint i = 0; i < G_N_ELEMENTS(buttons); i++) {
         gtk_widget_add_css_class(buttons[i], "nav");
         gtk_widget_set_focus_on_click(buttons[i], FALSE);
     }
-
     gtk_widget_set_visible(a->stop, FALSE);
     gtk_widget_set_tooltip_text(a->back, "Geri");
     gtk_widget_set_tooltip_text(a->forward, "İleri");
@@ -319,19 +277,29 @@ static void activate(GtkApplication *g, gpointer u) {
     gtk_widget_set_tooltip_text(home, "Ana sayfa");
     gtk_widget_set_tooltip_text(plus, "Yeni sekme");
 
-    gtk_box_append(GTK_BOX(nav), a->back);
-    gtk_box_append(GTK_BOX(nav), a->forward);
-    gtk_box_append(GTK_BOX(nav), a->reload);
-    gtk_box_append(GTK_BOX(nav), a->stop);
-    gtk_box_append(GTK_BOX(nav), home);
-
+    gtk_box_append(GTK_BOX(toolbar), a->back);
+    gtk_box_append(GTK_BOX(toolbar), a->forward);
+    gtk_box_append(GTK_BOX(toolbar), a->reload);
+    gtk_box_append(GTK_BOX(toolbar), a->stop);
+    gtk_box_append(GTK_BOX(toolbar), home);
     a->address = gtk_search_entry_new();
     gtk_widget_add_css_class(a->address, "address");
     gtk_widget_set_hexpand(a->address, TRUE);
     gtk_widget_set_tooltip_text(a->address, "Adres veya arama");
-    gtk_box_append(GTK_BOX(nav), a->address);
-    gtk_box_append(GTK_BOX(nav), plus);
-    gtk_header_bar_set_title_widget(GTK_HEADER_BAR(hb), nav);
+    gtk_box_append(GTK_BOX(toolbar), a->address);
+    gtk_box_append(GTK_BOX(toolbar), plus);
+
+    a->tabs = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 3);
+    gtk_widget_add_css_class(a->tabs, "tabs");
+    a->stack = gtk_stack_new();
+    gtk_widget_set_hexpand(a->stack, TRUE);
+    gtk_widget_set_vexpand(a->stack, TRUE);
+    gtk_stack_set_transition_type(GTK_STACK(a->stack), GTK_STACK_TRANSITION_TYPE_NONE);
+
+    gtk_box_append(GTK_BOX(root), toolbar);
+    gtk_box_append(GTK_BOX(root), a->tabs);
+    gtk_box_append(GTK_BOX(root), a->stack);
+    gtk_window_set_child(GTK_WINDOW(a->window), root);
 
     g_signal_connect(a->back, "clicked", G_CALLBACK(back_cb), a);
     g_signal_connect(a->forward, "clicked", G_CALLBACK(forward_cb), a);
@@ -340,15 +308,6 @@ static void activate(GtkApplication *g, gpointer u) {
     g_signal_connect(home, "clicked", G_CALLBACK(home_cb), a);
     g_signal_connect(plus, "clicked", G_CALLBACK(new_tab_cb), a);
     g_signal_connect(a->address, "activate", G_CALLBACK(address_activate), a);
-
-    GtkWidget *root = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    a->tabs = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 3);
-    gtk_widget_add_css_class(a->tabs, "tabs");
-    a->stack = gtk_stack_new();
-    gtk_widget_set_vexpand(a->stack, TRUE);
-    gtk_box_append(GTK_BOX(root), a->tabs);
-    gtk_box_append(GTK_BOX(root), a->stack);
-    gtk_window_set_child(GTK_WINDOW(a->window), root);
 
     new_tab(a, HOME_URL);
     gtk_window_present(GTK_WINDOW(a->window));
